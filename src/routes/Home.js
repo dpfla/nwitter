346fa6b4176cd2f00c652594abf88df182a8d5b3
dpfla <dpfla73@gmail.com>
2022-12-17 22:23:28 +0900

    firestore 연결 및 트윗 생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,40 @@
-import React from "react";
+import { dbService } from "fBase";
+import { addDoc, collection } from "firebase/firestore";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const[nweet, setNweet] = useState("");
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const docRef = await addDoc(collection(dbService, "nweets"), {
+              nweet,
+              createdAt: Date.now(),
+            });
+            setNweet("");
+            console.log("Document written with ID: ", docRef.id);
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+    };
+    const onChange = (event) => {
+        const { target: {value} } = event;
+        setNweet(value);
+    };
+
+    return(
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                    value={nweet} 
+                    type="text" 
+                    placeholder="What's on your mind?" 
+                    maxLength={120}
+                    onChange={onChange}
+                />
+                <input type="submit" value="Nweet" />
+            </form>
+        </div>
+    );
+};
 export default Home;
